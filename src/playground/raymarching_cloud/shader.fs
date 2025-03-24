bb50663786957dc984a4bf25524cc2564ae3085d
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2025-03-24 23:53:24 +0900

    feat: ライティングをつける

--- src/playground/raymarching_cloud/shader.fs
+++ src/playground/raymarching_cloud/shader.fs
@@ -17,6 +17,20 @@ float calc_dist(vec3 pos, float size)
   return length(pos) - size;
 }
 
+// 陰関数の微分により、球表面の任意座標における法線ベクトルを計算する
+vec3 get_normal(vec3 pos, float size)
+{
+  float v = 0.0001;
+  float d = calc_dist(pos, size);
+  return normalize(
+    vec3(
+      d - calc_dist(vec3(pos.x - v, pos.y,     pos.z    ), size),
+      d - calc_dist(vec3(pos.x,     pos.y - v, pos.z    ), size),
+      d - calc_dist(vec3(pos.x,     pos.y,     pos.z - v), size)
+    )
+  );
+}
+
 void main() {
   // -1.0 ~ 1.0 の空間を定義
   vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / min(u_resolution.x, u_resolution.y);
@@ -30,6 +44,9 @@ void main() {
   // 何もなければ黒色にする
   vec4 color = vec4(vec3(0.0), 1.0);
 
+  // 光源の位置
+  vec3 lightPos = normalize(vec3(1.0));
+
   for(int i = 0; i < raySteps; i++)
   {
     // 現在のrayの先端の位置が球に衝突していたら、色をつける
@@ -38,7 +55,10 @@ void main() {
 
     if(dist < 0.0001)
     {
-      color = vec4(vec3(1.0), 1.0);
+      // 光源の影響を計算
+      vec3 normal = get_normal(rayPos, radius);
+      float diff = dot(normal, lightPos);
+      color = vec4(vec3(1.0) * diff, 1.0);
       break;
     }
 
